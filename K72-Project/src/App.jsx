904335f697c3a3a4acba695e6c4ede1f3db3763f
Vikash Mishra <mishravikash0612@gmail.com>
2025-08-29 22:25:17 +0530

    Third Commit

--- K72-Project/src/App.jsx
+++ K72-Project/src/App.jsx
@@ -3,11 +3,15 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Agence from "./pages/Agence"
 import Projects from "./pages/Projects"
+import Navbar from './components/navigation/Navbar'
+import FullScreenNav from './components/navigation/FullScreenNav'
 
 const App = () => {
 
   return (
     <div >
+      <Navbar />
+      <FullScreenNav />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
